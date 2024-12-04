ea5d4e3ea7392d917707dc5ef11a787b3644bd91
Lucas Reis <lucasreisribeiro97@gmail.com>
2024-12-04 13:08:53 -0300

    refactor: :recycle: melhorias temporárias na autenticação

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->use([\Illuminate\Session\Middleware\StartSession::class]);
         $middleware->alias([
             'verified' => EnsureEmailIsVerified::class,
         ]);
